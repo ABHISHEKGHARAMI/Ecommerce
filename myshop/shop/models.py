2045c6ea6046c8223c0fc8237aea9596b2a38485
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-19 12:09:27 +0530

    Added the models for the product Category .

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# build the models for the category and the product for that
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+    
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+
+        
