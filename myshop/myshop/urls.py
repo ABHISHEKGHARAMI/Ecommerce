f6d67cce4d14edf3a1652d9092dc450d4ac74852
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-21 09:46:46 +0530

    .mod

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/',include('shop.urls',namespace='shop')),
+    path('',include('shop.urls',namespace='shop')),
 ]
 
 
