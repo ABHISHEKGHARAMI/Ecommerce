f500ee86dddaf150026c5477733bcfe142072a83
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-22 12:24:44 +0530

    Adding the views for the e commerce .

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -17,3 +17,21 @@ def cart_add(request,product_id):
                  override_quality=cd['override'])
         
     return redirect('cart:cart_detail')
+
+@require_POST
+def cart_remove(request,product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product,id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(
+        request,
+        'cart/detail.html',
+        {
+            'cart' : cart
+        }
+    )
