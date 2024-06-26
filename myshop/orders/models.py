38d19834dc34bebcad1fb06aac1bb37085e0b63c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-26 12:13:17 +0530

    Adding the order model for the shopping .

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from shop.models import Product
 # Create your models here.
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    
+    def __str__(self):
+        return f'Order {self.id}'
+    
+    
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
