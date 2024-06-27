6cf5a63d9994130e9e8dfdfbe79f7c19aa79bc26
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-27 14:11:06 +0530

    Adding the specific task  for the views to send email at th backend .

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 # Create your views here.
 
@@ -18,6 +19,9 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+
+            # launch asynchronous task
+            order_created.delay(order.id)
         return render(
             request,
             'orders/order/created.html',
