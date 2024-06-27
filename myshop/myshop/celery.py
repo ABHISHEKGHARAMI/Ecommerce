7cc9a2200487433d7cebc8b100b3e5a204b910c7
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-27 13:12:06 +0530

    .mod

--- myshop/myshop/celery.py
+++ myshop/myshop/celery.py
@@ -1,5 +1,5 @@
 import os
-from celery import Celery
+from .celery import Celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
