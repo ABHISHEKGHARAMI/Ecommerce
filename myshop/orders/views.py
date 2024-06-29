eee353fbc9dad7046e5617a75d343caa8d614c43
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-29 14:32:39 +0530

    Adding the payment process for the user .

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -22,13 +23,12 @@ def order_create(request):
 
             # launch asynchronous task
             order_created.delay(order.id)
-        return render(
-            request,
-            'orders/order/created.html',
-            {
-               'orders':order 
-            }
-        )
+            # set the order for the session
+            request.session['order_id'] =  order.id
+            # redirect for the payment
+            return redirect(reverse('payment:process')) 
+            
+        
     else:
         form = OrderCreateForm()
     return render(
