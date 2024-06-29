6247753377b858ee29895f40988762874090f55e
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-29 15:17:34 +0530

    .mod

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -56,5 +56,5 @@ def payment_process(request):
 def payment_completed(request):
     return render(request,'payment/completed.html')
 
-def payment_cancelled(request):
-    return render(request,'payment/cancelled.html')
+def payment_canceled(request):
+    return render(request,'payment/canceled.html')
