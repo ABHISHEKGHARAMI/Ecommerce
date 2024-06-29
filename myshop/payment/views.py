8c4c4f740d22b81faf7a6ca8820e36a88229927b
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-29 15:08:58 +0530

    Adding the after payment and payment cancellation views.

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -52,3 +52,9 @@ def payment_process(request):
             'payment/process.html',
             locals()
         )
+        
+def payment_completed(request):
+    return render(request,'payment/completed.html')
+
+def payment_cancelled(request):
+    return render(request,'payment/cancelled.html')
