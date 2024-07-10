2aceecee5559e60eb8d6aeb98ef6039790bffac6
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-10 12:40:40 +0530

    Adding the webhook for the repo.

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from . import webhooks
+
 app_name = 'payment'
 
 
@@ -9,4 +11,5 @@ urlpatterns = [
     path('process/',views.payment_process,name="process"),
     path('completed/',views.payment_completed,name="completed"),
     path('canceled/',views.payment_canceled,name="canceled"),
+    path('webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
 ]
