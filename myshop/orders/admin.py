ab43b64a9ac61d7d7344170aafc1c2d390d3b341
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-16 13:26:59 +0530

    .mod

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 from .models import Order,OrderItem
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
     
-    
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+        return ''
+    order_payment.payment.short_description = 'Stripe payment'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid','created', 'updated']
+                    'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
     
     list_filter = ['paid','created','updated']
     inlines = [OrderItemInline]
