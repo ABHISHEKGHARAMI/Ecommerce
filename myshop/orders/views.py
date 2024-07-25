2648e7186fadf87efd456a3c3c7d890d2799b974
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-25 17:08:28 +0530

    .mod

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -5,6 +5,10 @@ from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 # Create your views here.
 
@@ -52,3 +56,16 @@ def admin_order_detail(request, order_id):
             'order' : order
         }
     )
+
+
+@staff_member_required
+def admin_order_pdf(request,order_id):
+    order = get_object_or_404(Order,id = order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+    stylesheets=[weasyprint.CSS(
+        settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
