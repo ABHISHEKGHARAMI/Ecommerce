ff18c2abc288034c590b0be87c6d36f11b366d59
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-31 13:48:27 +0530

    .mod

---
+++ myshop/payment/tasks.py
@@ -0,0 +1,46 @@
+from io import BytesIO
+from celery import shared_task
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
+from django.core.mail import EmailMessage
+from django.conf import settings
+from orders import Order
+
+@shared_task
+def payment_completed(order_id):
+    order = Order.objects.get(id=order_id)
+    
+    subject = f"Tea shop - order : {order.id}"
+    
+    message = 'Please find the attachment with the invoice.'
+    
+    # setting the email
+    email = EmailMessage(subject,
+                         message,
+                         'abhishekgharami1998@gmail.com',
+                         [order.email])
+    # generate the pdf for the invoice
+    html = render_to_string('orders/order/pdf.html',
+                            {'order':order})
+    
+    out = BytesIO()
+    
+    # creating the pdf using the xhtml2pdf
+    
+    pisa_status = pisa.CreatePDF(
+        html,
+        dest=out,
+        link_callback = lambda uri,rel : f'setting.STATIC_ROOT/{uri}'
+    )
+    
+    if pisa_status.err:
+        return False
+    
+    # attach the css for style
+    email.attach(f'order_{order.id}.pdf',
+                 out.getvalue(),
+                 'application/pdf')
+    
+    # sending the email
+    email.send()
+    
--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
-#from .tasks import payment_completed
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -40,7 +40,7 @@ def stripe_webhook(request):
             order.stripe_id = session.payment_intent
             order.save()
             # launch asynchronous task
-            #payment_completed.delay(order.id)
+            payment_completed.delay(order.id)
 
     
     return HttpResponse(status=200)
