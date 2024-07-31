ac6b8b4fc52bad8594fa705205ee88713cbdf4e2
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-31 13:57:43 +0530

    .mod

--- myshop/payment/tasks.py
+++ myshop/payment/tasks.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 from xhtml2pdf import pisa
 from django.core.mail import EmailMessage
 from django.conf import settings
-from orders import Order
+from orders.models import Order
 
 @shared_task
 def payment_completed(order_id):
